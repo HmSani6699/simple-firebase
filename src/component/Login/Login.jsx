0d2f4c55c1f767b5573928eb394e7b31c8c78ab6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-28 08:28:27 +0600

    ad to log out methoad

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,24 +1,45 @@
 // import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app);
-    console.log(app);
     const provider = new GoogleAuthProvider();
 
-    const heandlGoogleAuth = () => {
+    const heandlGoogleSignIn = () => {
         signInWithPopup(auth, provider)
+            .then(result => {
+                const loginUser = result.user;
+                console.log(loginUser);
+                setUser(loginUser)
+            })
+            .catch(error => {
+                console.log('error', error.message);
+            })
+    }
+
+    const handalGoogleSignOut=()=>{
+        signOut(auth)
         .then(result=>{
-            const user=result.user;
-            console.log(user);
+            console.log(result);
+            setUser(null)
         })
         .catch(error=>{
-            console.log('error', error.message);
+            console.log(error);
         })
     }
     return (
         <div>
-            <button onClick={heandlGoogleAuth}>Login</button>
+            <button onClick={heandlGoogleSignIn}>Login</button>
+            <button onClick={handalGoogleSignOut}>Log Out</button>
+            {user &&
+                <div>
+                    <h2>User {user.displayName}</h2>
+                    <p>Email {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
