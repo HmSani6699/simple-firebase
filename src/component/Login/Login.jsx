43b5251521760868ee201997367d8ac0addfd710
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-29 10:13:59 +0600

    add a new auth part

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,67 +1,93 @@
-// import React from 'react';
-import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+// // import React from 'react';
+// import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+// import app from "../../firebase/firebase.init";
+// import { useState } from "react";
+
+
+// const Login = () => {
+
+
+//     const [user, setUser] = useState(null)
+//     const auth = getAuth(app);
+//     const googleProvider = new GoogleAuthProvider();
+//     const githubProvider = new GithubAuthProvider();
+
+//     const heandlGoogleSignIn = () => {
+//         signInWithPopup(auth, googleProvider)
+//             .then(result => {
+//                 const loginUser = result.user;
+//                 console.log(loginUser);
+//                 setUser(loginUser)
+//             })
+//             .catch(error => {
+//                 console.log('error', error.message);
+//             })
+//     }
+
+//     const heandlGithubSignIn = () => {
+//         signInWithPopup(auth, githubProvider)
+//             .then(result => {
+//                 const loginUser = result.user;
+//                 setUser(loginUser)
+//             })
+//             .then(error => {
+//                 console.log(error);
+//             })
+//     }
+
+//     const handalGoogleSignOut = () => {
+//         signOut(auth)
+//             .then(result => {
+//                 console.log(result);
+//                 setUser(null)
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//             })
+//     }
+//     return (
+//         <div>
+//             {
+//                 user ?
+//                     <button onClick={handalGoogleSignOut}>Log Out</button> :
+//                     <div>
+
+//                         <button onClick={heandlGoogleSignIn}>Google Login</button>
+//                         <button onClick={heandlGithubSignIn}>Github Login</button>
+//                     </div>
+//             }
+//             {user &&
+//                 <div>
+//                     <h2>User {user.displayName}</h2>
+//                     <p>Email {user.email}</p>
+//                     <img src={user.photoURL} alt="" />
+//                 </div>
+//             }
+//         </div>
+//     );
+// };
+
+// export default Login;
+
+
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import app from "../../firebase/firebase.init";
-import { useState } from "react";
+
+
 const Login = () => {
-    const [user, setUser] = useState(null)
+
     const auth = getAuth(app);
-    const googleProvider = new GoogleAuthProvider();
-    const githubProvider = new GithubAuthProvider();
+    const googleProvider=new GoogleAuthProvider();
 
-    const heandlGoogleSignIn = () => {
-        signInWithPopup(auth, googleProvider)
-            .then(result => {
-                const loginUser = result.user;
-                console.log(loginUser);
-                setUser(loginUser)
-            })
-            .catch(error => {
-                console.log('error', error.message);
-            })
-    }
-
-    const heandlGithubSignIn = () => {
-        signInWithPopup(auth, githubProvider)
-            .then(result => {
-                const loginUser = result.user;
-                setUser(loginUser)
-            })
-            .then(error => {
-                console.log(error);
-            })
-    }
-
-    const handalGoogleSignOut = () => {
-        signOut(auth)
-            .then(result => {
-                console.log(result);
-                setUser(null)
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
+const handleGoogleLogin=()=>{
+    console.log('ami gooogle mama');
+}
     return (
         <div>
-            {
-                user ?
-                    <button onClick={handalGoogleSignOut}>Log Out</button> :
-                    <div>
-
-                        <button onClick={heandlGoogleSignIn}>Google Login</button>
-                        <button onClick={heandlGithubSignIn}>Github Login</button>
-                    </div>
-            }
-            {user &&
-                <div>
-                    <h2>User {user.displayName}</h2>
-                    <p>Email {user.email}</p>
-                    
-                    <img src={user.photoURL} alt="" />
-                </div>
-            }
+            <button onClick={handleGoogleLogin}>GoogleLogin</button>
         </div>
     );
 };
 
 export default Login;
+
